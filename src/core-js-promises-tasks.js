47a892d6b73651ed8068c590c81caf261d3431ae
whowouldwin <leranetwork@gmail.com>
2025-04-14 18:44:02 +0300

    feat: implement getPromiseResult

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -39,8 +39,14 @@ function getPromise(number) {
  * Promise.resolve('success') => promise that will be fulfilled with 'success' value
  * Promise.reject('fail')     => promise that will be fulfilled with 'fail' value
  */
-function getPromiseResult(/* source */) {
-  throw new Error('Not implemented');
+function getPromiseResult(source) {
+  return source
+    .then(function Success() {
+      return 'success';
+    })
+    .catch(function Catch() {
+      return 'fail';
+    });
 }
 
 /**
