ac0f4763dda7fd7353315d0a7e9f07f806b92ecd
whowouldwin <leranetwork@gmail.com>
2025-04-14 20:06:49 +0300

    feat: implement queuePromises

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -142,8 +142,17 @@ function getAllResult(promises) {
  * [promise1, promise4, promise3] => Promise.resolved('104030')
  * [promise1, promise4, promise3, promise2] => Promise.resolved('10403020')
  */
-function queuePromises(/* promises */) {
-  throw new Error('Not implemented');
+function queuePromises(promises) {
+  let result = '';
+  return promises
+    .reduce((chain, currentPromise) => {
+      return chain.then(() => {
+        return currentPromise.then((value) => {
+          result += String(value);
+        });
+      });
+    }, Promise.resolve())
+    .then(() => result);
 }
 
 module.exports = {
